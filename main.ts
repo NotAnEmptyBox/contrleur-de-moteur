690821cde07ef7a04fab0fb0ff3d071bdd817321
NotAnEmptyBox <93144513+NotAnEmptyBox@users.noreply.github.com>
2021-12-08 16:00:52 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+function Rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function Blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function Green () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+basic.forever(function () {
+    if (550 < pins.analogReadPin(AnalogPin.P0)) {
+        Green()
+    } else if (450 > pins.analogReadPin(AnalogPin.P0)) {
+        Rouge()
+    } else {
+        Blanc()
+    }
+})
